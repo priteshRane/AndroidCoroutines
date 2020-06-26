41b82a6bfe8aa537c00141617885f2c34b15d278
Pritesh_PC <priteshrane01@gmail.com>
2020-06-26 16:47:35 +0530

    Nav graph is added for fragment

--- app/src/main/java/com/ransoft/androidcoroutines/simplifiedcoading/ui/QuotesActivity.kt
+++ app/src/main/java/com/ransoft/androidcoroutines/simplifiedcoading/ui/QuotesActivity.kt
@@ -3,6 +3,7 @@ package com.ransoft.androidcoroutines.simplifiedcoading.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ransoft.androidcoroutines.R
+import kotlinx.android.synthetic.main.activity_quotes.*
 
 class QuotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
