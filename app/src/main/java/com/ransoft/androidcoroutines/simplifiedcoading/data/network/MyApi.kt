e734ad269d4a665872e33b91c19e656b701e56fe
Pritesh_PC <priteshrane01@gmail.com>
2020-06-26 19:54:23 +0530

    Async

--- app/src/main/java/com/ransoft/androidcoroutines/simplifiedcoading/data/network/MyApi.kt
+++ app/src/main/java/com/ransoft/androidcoroutines/simplifiedcoading/data/network/MyApi.kt
@@ -1,5 +1,6 @@
 package com.ransoft.androidcoroutines.simplifiedcoading.data.network
 
+import com.ransoft.androidcoroutines.simplifiedcoading.data.models.Movie
 import com.ransoft.androidcoroutines.simplifiedcoading.data.network.responses.QuotesResponse
 import retrofit2.Call
 import retrofit2.Response
@@ -11,14 +12,17 @@ interface MyApi {
 //    @GET("quotes")
 //    fun getQuotes() : Call<QuotesResponse>
 
-    @GET("quotes")
+    @GET("mvvm/quotes")
     suspend fun getQuotes(): Response<QuotesResponse>
+
+    @GET("recyclerview/movies")
+    suspend fun getMovies() : Response<List<Movie>>
 
     companion object {
         operator fun invoke(): MyApi {
             return Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl("https://api.simplifiedcoding.in/course-apis/mvvm/")
+                .baseUrl("https://api.simplifiedcoding.in/course-apis/")
                 .build()
                 .create(MyApi::class.java)
         }
--- app/src/main/java/com/ransoft/androidcoroutines/simplifiedcoading/ui/QuotesViewModel.kt
+++ app/src/main/java/com/ransoft/androidcoroutines/simplifiedcoading/ui/QuotesViewModel.kt
@@ -4,23 +4,40 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ransoft.androidcoroutines.simplifiedcoading.data.models.Movie
 import com.ransoft.androidcoroutines.simplifiedcoading.data.models.Quote
 import com.ransoft.androidcoroutines.simplifiedcoading.data.network.MyApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class QuotesViewModel : ViewModel() {
+    private val TAG = "QuoteViewModel"
     val quotes : LiveData<List<Quote>> = MutableLiveData()
 
     init {
         viewModelScope.launch {
             quotes as MutableLiveData
             quotes.value = getQuotes()
+
+            // Using async
+//            val movies = async { getMovies() }
+//            val value = movies.await()
         }
     }
 
     private suspend fun getQuotes(): List<Quote>? {
-        return withContext(Dispatchers.IO) { MyApi().getQuotes().body()?.quotes }
+        return withContext(Dispatchers.IO) {
+            android.util.Log.i(TAG, "Getting Quotes")
+            MyApi().getQuotes().body()?.quotes
+        }
+    }
+
+    private suspend fun getMovies(): List<Movie>? {
+        return withContext(Dispatchers.IO) {
+            android.util.Log.e(TAG, "Getting Movies")
+            MyApi().getMovies().body()
+        }
     }
 }
